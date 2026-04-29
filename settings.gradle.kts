c4165da627fe69c4cd21fe3192ed8c3bc820f247
ieatglu3 <gluee4at3r@gmail.com>
2026-04-29 18:33:43 -0400

    improve build structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,13 @@ include("api", "platform-util")
 include("spigot")
 
 include("example", "example:spigot")
+
+setChildrenProjectNames(rootProject.name, rootProject)
+
+fun setChildrenProjectNames(prefix: String, parent: ProjectDescriptor) {
+  for (it in parent.children)
+  {
+    it.name = "${prefix}-${it.name}"
+    setChildrenProjectNames(prefix, it)
+  }
+}
